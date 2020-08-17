4525132031232bf42495ed818a3e1e8dc7adda78
Gabriel Kruger <gabriel.silva@iv2.com.br>
2020-08-17 15:20:36 -0300

    firt commit

---
+++ script.js
@@ -0,0 +1,82 @@
+var estrutura = document.getElementById('body')
+var cont=0;
+var links = []
+const xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://api.github.com/users/GabrielKruger-IV2/repos")
+xhr.send();
+
+xhr.onreadystatechange = function () {
+    if (xhr.status == 200 && xhr.readyState == 4) {
+
+        const result = JSON.parse(xhr.responseText);
+        estrutura.innerHTML+= `<div id="Titulo">
+        <h1>Meu portfólio</h1>
+        </div>`
+        for (i = 0; i < result.length; i++) {
+            if(cont==0){
+            estrutura.innerHTML += `    
+            
+            <div id="div" class="left">
+                <table id="table" >
+                    <tr >
+                    
+                        <td id="tdNome"><h3 id="nome">Nome:${result[i].name}</h3></td>
+                        <td class="tdA"><a href="${result[i].html_url}" id="linkPagina">Link</a></td>
+                    
+                </tr>
+    
+                <tr>
+                    
+                    <td id="tdLing"><img src='images/${result[i].language}.png' width='100px' height='100px'><p id="linguagem">Linguagem principal:${result[i].language}</p></td>
+                    <td class="tdA"><button class="btn" onclick="copiar(${i})">Clonar</button></td>
+                </tr>
+    
+        
+                </table>
+            </div>`;
+        cont=1;
+        links[i] = result[i].clone_url;
+    }else{
+        estrutura.innerHTML += `    
+        
+        
+        <div id="div" class="right">
+            <table id="table" >
+                <tr >
+                
+                    <td id="tdNome"><h3 id="nome">Nome:${result[i].name}</h3></td>
+                    <td class="tdA"><a href="${result[i].html_url}" id="linkPagina">Link</a></td>
+                
+            </tr>
+
+            <tr>
+                
+                <td id="tdLing"><img src='images/${result[i].language}.png' width='100px' height='100px'><p id="linguagem">Linguagem principal:${result[i].language}</p></td>
+                <td class="tdA"><button class="btn" onclick="copiar(${i})">Clonar</button></td>
+            </tr>
+
+    
+            </table>
+        </div>
+    `;
+        cont=0;
+        links[i] = result[i].clone_url;
+        }
+
+
+        }
+    }
+}
+
+
+function copiar(e) {
+    const input= document.createElement("input");
+    input.value = links[e];
+    input.id = "input";
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand("copy");
+    input.remove();
+    alert("Link copiado");
+  }
